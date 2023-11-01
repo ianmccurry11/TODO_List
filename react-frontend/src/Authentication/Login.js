53af00cdc2ccb1e25e541f6187d9594d0f981b6a
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-11-01 11:22:43 -0700

    Addded prettier and eslint

--- react-frontend/src/Authentication/Login.js
+++ react-frontend/src/Authentication/Login.js
@@ -1,61 +1,56 @@
-import axios from 'axios';
-import React, {useState} from 'react';
+import axios from "axios";
+import React, { useState } from "react";
 
 function Login() {
-
-    const [user, setUser] = useState(
-     {
-        username: "",
-        password: "",
-     }
-  );
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
   const [login, setLogin] = useState(false);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
     if (name === "password")
-      setUser(
-        {username: user['username'], password: value}
-      );
-    else     
-      setUser(
-        {username: value, password: user['password']}   
-      );
-  }
+      setUser({ username: user["username"], password: value });
+    else setUser({ username: value, password: user["password"] });
+  };
 
-    const submitForm = () => {
-        console.log(user);
-        alert("You are submitting " + user.username + " " + user.password)
-        setUser({username: '', password: ''});
-        axios.post('http://localhost:8000/login', user)
-        .then((result) => {
+  const submitForm = () => {
+    console.log(user);
+    alert("You are submitting " + user.username + " " + user.password);
+    setUser({ username: "", password: "" });
+    axios
+      .post("http://localhost:8000/login", user)
+      .then((result) => {
         setLogin(true);
-        })
-        .catch((error) => {
-            error = new Error();
-        });
-    }
+      })
+      .catch((error) => {
+        error = new Error();
+      });
+  };
 
   return (
     <>
-        <h1>Login</h1>
-        <form>
-            <label htmlFor="username">Username</label>
-            <input
-                type="text"
-                name="username"
-                id="username"
-                value={user.username}
-                onChange={handleChange} />
-            <label htmlFor="password">Password</label>
-            <input
-                type="text"
-                name="password"
-                id="password"
-                value={user.password}
-                onChange={handleChange} />
-            <input type="button" value="Submit" onClick={submitForm} />
-        </form>
+      <h1>Login</h1>
+      <form>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={user.username}
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="text"
+          name="password"
+          id="password"
+          value={user.password}
+          onChange={handleChange}
+        />
+        <input type="button" value="Submit" onClick={submitForm} />
+      </form>
     </>
   );
 }
