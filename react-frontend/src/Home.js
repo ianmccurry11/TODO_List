24fd46259ccacba20c077d71fb0989f9659d5378
Jacob M <114640505+JMestre32@users.noreply.github.com>
2023-11-30 16:20:45 -0800

    Change landing page to align with theme

--- react-frontend/src/Home.js
+++ react-frontend/src/Home.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ButtonAppBar from './Navbar';
 
-const HomePage = () => (
-  <>
-    <ButtonAppBar />
-    <header>
-      <h1>My Simple React Home Page</h1>
-    </header>
-    <main>
-      <h1>Mission Log</h1>
-    </main>
-  </>
-);
+const HomePage = () => {
+  const [text, setText] = useState('');
+  const fullText = 'Welcome to Mission Log';
+
+  useEffect(() => {
+    if (text.length < fullText.length) {
+      setTimeout(() => setText(fullText.slice(0, text.length + 1)), 100);
+    }
+  }, [text]);
+
+  return (
+    <div style={{
+      minHeight: '100vh',
+      backgroundImage: 'radial-gradient(circle at center, #0a4700 0%, #002400 80%, #000000 100%)',
+    }}
+    >
+      <ButtonAppBar />
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      >
+        <main>
+          <h1 style={{ color: '#1bff80', fontFamily: '"Courier New", Courier, monospace' }}>{text}</h1>
+        </main>
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
